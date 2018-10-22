bbf77e84873e5c22d012615374415e8149b6bab1
Mazen Morgan <morgan.mazen42@gmail.com>
2018-10-22 22:20:49 +0200

    Writting in the bitmap

--- DrawingFractalImages/DrawingFractalImages/Bitmap.cpp
+++ DrawingFractalImages/DrawingFractalImages/Bitmap.cpp
@@ -1,10 +1,12 @@
+
 #include "stdafx.h"
 #include "Bitmap.h"
 #include "BitmapInfoHeader.h"
 #include "BitmapFileHeader.h"
+#include <fstream>
 
 using namespace BitmapContainer;
-
+using namespace std;
 namespace BitmapContainer
 {
 	Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height), m_pPixel(new uint8_t[width * height * 3]{}) { }
@@ -20,7 +22,18 @@ namespace BitmapContainer
 
 		infoHeader.width = m_width;
 		infoHeader.height = m_height;
-		return false;
+
+		ofstream file;
+		file.open(filename, ios::out | ios::binary);
+		if (!file) { return false; }
+
+		file.write((char *)&fileHeader, sizeof(fileHeader));
+		file.write((char *)&infoHeader, sizeof(infoHeader));
+		file.write((char *)m_pPixel.get(), sizeof(fileHeader));
+
+		file.close();
+		if (!file) { return false; }
+		return true;
 	}
 
 	void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue)
