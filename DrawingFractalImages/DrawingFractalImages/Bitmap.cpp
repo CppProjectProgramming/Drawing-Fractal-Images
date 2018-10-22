b3146d423056fa8c111754e27ddbde460b5c13b9
Mazen Morgan <morgan.mazen42@gmail.com>
2018-10-22 21:17:44 +0200

    setting Bitmap header values

--- DrawingFractalImages/DrawingFractalImages/Bitmap.cpp
+++ DrawingFractalImages/DrawingFractalImages/Bitmap.cpp
@@ -1,14 +1,25 @@
 #include "stdafx.h"
 #include "Bitmap.h"
+#include "BitmapInfoHeader.h"
+#include "BitmapFileHeader.h"
 
+using namespace BitmapContainer;
 
 namespace BitmapContainer
 {
-	Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height), m_pPixel(new uint8_t[width * height * 3]){ }
+	Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height), m_pPixel(new uint8_t[width * height * 3]{}) { }
 
 
 	bool Bitmap::write(string filename)
 	{
+		BitmapFileHeader fileHeader;
+		BitmapInfoHeader infoHeader;
+
+		fileHeader.fileSize = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) + m_width * m_height * 3;
+		fileHeader.dataOffset = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
+
+		infoHeader.width = m_width;
+		infoHeader.height = m_height;
 		return false;
 	}
 
