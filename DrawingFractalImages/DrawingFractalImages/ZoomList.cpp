7c828a796b83706f523f6bb13f2fa018e5ff60a0
Mazen Morgan <morgan.mazen42@gmail.com>
2018-11-09 16:28:26 +0200

    Centering and Scaling

--- DrawingFractalImages/DrawingFractalImages/ZoomList.cpp
+++ DrawingFractalImages/DrawingFractalImages/ZoomList.cpp
@@ -1,15 +1,23 @@
 #include "stdafx.h"
 #include "ZoomList.h"
 
-
+using namespace std;
 ZoomList::ZoomList(int width, int height) : m_width(width), m_height(height)
 {
+	
 }
 
 
 void ZoomList::add(const Zoom& zoom)
 {
 	zooms.push_back(zoom);
+
+	m_xCenter += (zoom.x - m_width / 2) *m_scale;
+	m_yCenter += (zoom.y - m_height / 2) *m_scale;
+
+	m_scale *= zoom.scale;
+
+	cout << m_xCenter << ", " << m_yCenter << ", " << m_scale << endl;
 }
 
 pair<double, double> ZoomList::doZoom(int x, int y)
--- DrawingFractalImages/DrawingFractalImages/ZoomList.h
+++ DrawingFractalImages/DrawingFractalImages/ZoomList.h
@@ -11,12 +11,17 @@ using namespace BitmapContainer;
 class ZoomList
 {
 private:
+	double m_xCenter = 0;
+	double m_yCenter = 0;
+	double m_scale = 1.0;
+
 	int m_width = 0;
 	int m_height = 0;
 
 	vector<Zoom> zooms;
 
 public:
+	ZoomList() {}
 	ZoomList(int width, int height);
 	void add(const Zoom& zoom);
 	pair<double, double> doZoom(int x, int y);
